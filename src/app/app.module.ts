5128205ed180363e97fc9e54fce23c874e120790
Darren Nienaber <staysignal@outlook.com>
2022-02-17 14:16:54 +0200

    updates to file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NatoursComponent } from './natours/natours.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NatoursComponent
   ],
   imports: [
     BrowserModule,
